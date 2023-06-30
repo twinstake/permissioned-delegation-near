9f47fc1617bb9e5efc35d26cf6ebb4c2e6dd6908
Lautaro Emanuel <emlautarom1@gmail.com>
2023-06-30 19:24:47 +0000

    Deleted 'staking-pool-factory' and 'whitelist'
    - Updated README

--- staking-pool-factory/build.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-set -e
-
-RUSTFLAGS='-C link-arg=-s' cargo build --target wasm32-unknown-unknown --release
-cp target/wasm32-unknown-unknown/release/staking_pool_factory.wasm ./res/
-wasm-opt -Oz --signext-lowering ./res/staking_pool_factory.wasm -o ./res/staking_pool_factory.wasm
-
